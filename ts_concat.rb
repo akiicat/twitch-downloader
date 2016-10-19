4e2321c8456a72375be04006bbdce971ba37546e
aaaa1379 <aaaa1379@gmail.com>
2016-10-20 05:24:40 +0800

    fix read big file error change to read line

--- ts_concat.rb
+++ ts_concat.rb
@@ -7,8 +7,8 @@ file       = File.open(ARGV[0], "wb")
 vedio_list = ARGV[1..-1]
 
 vedio_list.each do |vname|
-  File.open(vname, "r") { |f|
-    file.write(f.read)
+  File.open(vname, "r").each_line { |line|
+    file.write(line)
   }
 end
 
